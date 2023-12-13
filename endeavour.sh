38a47c71726bb4caee4ef60b62cf81c4f39a1387
kjooleng <138017011+kjooleng@users.noreply.github.com>
2023-12-13 21:52:55 +0800

    Update endeavour.sh

--- endeavour.sh
+++ endeavour.sh
@@ -19,3 +19,16 @@ sudo pacman -S fcitx5-im fcitx5-chinese-addons --noconfirm
 
 # other apps
 sudo pacman -S vlc filezilla openvpn networkmanager-openvpn x11vnc parcellite --noconfirm
+
+#Chaotic-AUR
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'  --noconfirm
+
+# configure pacman.conf
+sudo cat <<EOF >> /etc/pacman.conf
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
+EOF
+
+sudo pacman -Syu
