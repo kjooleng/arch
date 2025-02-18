3ba9636239a172d47f0d463a4fe154f25bb94ae9
kjooleng <138017011+kjooleng@users.noreply.github.com>
2025-02-19 02:53:09 +0800

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -33,7 +33,7 @@ echo "/swapfile none swap defaults 0 0" >> /etc/fstab
 
 
 #Chaotic-AUR
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --recv-key 3056513887B78AEB 
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'  --noconfirm
 
