6e63674e9bee7cd9cde61e23d724948b94ef4d7a
kjooleng <138017011+kjooleng@users.noreply.github.com>
2025-02-19 19:50:22 +0800

    Add files via upload

---
+++ others.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+arch-chroot /mnt pacman -S chaotic-aur/yay --noconfirm
+arch-chroot /mnt yay -S chaotic-aur/pamac --noconfirm
+arch-chroot /mnt yay -S chaotic-aur/appimagelauncher --noconfirm
+arch-chroot /mnt yay -S chaotic-aur/p7zip-gui --noconfirm
+arch-chroot /mnt yay -S chaotic-aur/anydesk --noconfirm
+arch-chroot /mnt yay -S chaotic-aur/microsoft-edge-stable-bin --noconfirm
+arch-chroot /mnt yay -S chaotic-aur/brave-bin --noconfirm
+#arch-chroot /mnt yay -S chaotic-aur/firedragon --noconfirm
+#arch-chroot /mnt yay -S chaotic-aur/librewolf --noconfirm
+#arch-chroot /mnt yay -S chaotic-aur/icecat --noconfirm
+#arch-chroot /mnt yay -S chaotic-aur/floorp --noconfirm
+#arch-chroot /mnt yay -S chaotic-aur/google-chrome --noconfirm
+#arch-chroot /mnt yay -S chaotic-aur/mercury-browser-avx-bin --noconfirm
+#arch-chroot /mnt yay -S chaotic-aur/waterfox-bin --noconfirm
+
+#no need for running vm
+
+#sudo pacman -S chaotic-aur/youplay --noconfirm
+#sudo pacman -S chaotic-aur/spotify --noconfirm
+
+arch-chroot /mnt yay -S chaotic-aur/video-downloader --noconfirm
+arch-chroot /mnt yay -S chaotic-aur/xorgxrdp --noconfirm
+arch-chroot /mnt pacman -S bitwarden --noconfirm
+arch-chroot /mnt pacman -S clipgrab --noconfirm
+
+#install firewall
+arch-chroot /mnt yay -S firewalld --noconfirm
+sudo arch-chroot /mnt systemctl enable firewalld
+sudo arch-chroot /mnt systemctl start firewalld
+
+# tlp install
+sudo arch-chroot /mnt pacman -S tlp tlp-rdw --noconfirm
+sudo arch-chroot /mnt systemctl enable tlp.service
+sudo arch-chroot /mnt systemctl enable NetworkManager-dispatcher.service
+sudo arch-chroot /mnt systemctl mask systemd-rfkill.service systemd-rfkill.socket
---
+++ others2.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+sudo pacman -S chaotic-aur/yay --noconfirm
+yay -S chaotic-aur/pamac --noconfirm
+yay -S chaotic-aur/appimagelauncher --noconfirm
+yay -S chaotic-aur/p7zip-gui --noconfirm
+yay -S chaotic-aur/anydesk --noconfirm
+yay -S chaotic-aur/microsoft-edge-stable-bin --noconfirm
+yay -S chaotic-aur/brave-bin --noconfirm
+yay -S chaotic-aur/firedragon --noconfirm
+yay -S chaotic-aur/librewolf --noconfirm
+#yay -S chaotic-aur/icecat --noconfirm
+#yay -S chaotic-aur/floorp --noconfirm
+#yay -S chaotic-aur/google-chrome --noconfirm
+#yay -S chaotic-aur/mercury-browser-avx-bin --noconfirm
+#yay -S chaotic-aur/waterfox-bin --noconfirm
+
+#no need for running vm
+
+#sudo pacman -S chaotic-aur/youplay --noconfirm
+#sudo pacman -S chaotic-aur/spotify --noconfirm
+
+yay -S chaotic-aur/video-downloader --noconfirm
+yay -S chaotic-aur/xorgxrdp --noconfirm
+sudo pacman -S bitwarden --noconfirm
+sudo pacman -S clipgrab --noconfirm
+
+#install firewall
+yay -S firewalld --noconfirm
+systemctl enable firewalld
+systemctl start firewalld
+
+# tlp install
+sudo pacman -S tlp tlp-rdw --noconfirm
+sudo systemctl enable tlp.service
+sudo systemctl enable NetworkManager-dispatcher.service
+sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket
