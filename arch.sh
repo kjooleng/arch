175e3ecd6f30be7687614816b303662ebb2d7a64
kjooleng <138017011+kjooleng@users.noreply.github.com>
2025-02-20 13:08:34 +0800

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -132,7 +132,7 @@ sudo systemctl enable tlp.service
 sudo systemctl enable NetworkManager-dispatcher.service
 sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket
 
-yay -S slimbookbattery --noconfirm
+yay -S slimbookbattery
 
 #yay -S knock-bin --noconfirm
 
