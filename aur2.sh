06588bc18057588ff9d65ea62f350673941057b9
kjooleng <138017011+kjooleng@users.noreply.github.com>
2025-02-20 23:29:49 +0800

    Update aur2.sh

--- aur2.sh
+++ aur2.sh
@@ -3,9 +3,9 @@
 sudo pacman-key --recv-key 72BF227DD76AE5BF
 sudo pacman-key --lsign-key 72BF227DD76AE5BF
 
-echo "  " >> /mnt/etc/pacman.conf
-echo "[andontie-aur]" >> /mnt/etc/pacman.conf
-echo "Server = https://aur.andontie.net/\$arch" >> /mnt/etc/pacman.conf
+echo "  " >> /etc/pacman.conf
+echo "[andontie-aur]" >> /etc/pacman.conf
+echo "Server = https://aur.andontie.net/\$arch" >> /etc/pacman.conf
 
 sudo pacman -Syu
 
@@ -18,8 +18,8 @@ sudo pacman-key --lsign-key 3056513887B78AEB
 #sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 
-echo "  " >> /mnt/etc/pacman.conf
-echo "[chaotic-aur]" >> /mnt/etc/pacman.conf
-echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /mnt/etc/pacman.conf
+echo "  " >> /etc/pacman.conf
+echo "[chaotic-aur]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
 
 sudo pacman -Sy
